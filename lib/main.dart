569ea707c73988490765fb885601063c936bae13
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-11-06 01:23:25 +0530

    Feat: Add a splash loading screen The loading screen is just the flutter logo, will need more work once the design for it is ready

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:raag/provider/theme.dart';
 import 'package:raag/model/strings.dart';
 import 'package:raag/view/home_scaffold.dart';
+import 'dart:async';
 
 import 'DarkThemeProvider.dart';
 
@@ -16,6 +17,33 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+          ()=>Navigator.pushReplacement(context,
+                                        MaterialPageRoute(builder:
+                                                          (context) => 
+                                                          HomeScaffold()
+                                                         )
+                                       )
+         );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
 }
 
 class _MyAppState extends State<MyApp> {
@@ -44,7 +72,8 @@ class _MyAppState extends State<MyApp> {
               debugShowCheckedModeBanner: false,
               theme: Styles.themeData(themeChangeProvider.darkTheme, context),
               title: title,
-              home: HomeScaffold());
+              home: MyHomePage(),
+              );
         },
       ),
     );
