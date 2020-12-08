4c78e63d18f7ceb330135fbab01d4f4fab8b4565
YUVRAJ SINGH YADAV <73466487+yuvi0069@users.noreply.github.com>
2020-12-08 22:17:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    return return Hero(
+    return Hero(
         tag: "hello",
         child: Center(
             child: Container(
