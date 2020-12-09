3dfc52aa27c81b770de957dd1774c2d34c9e6f91
Aditya Gupta <53346087+xcyberpunkx0@users.noreply.github.com>
2020-12-09 20:40:23 +0530

    Added ThemeButton

---
+++ lib/widgets/ThemeButton.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:animated_icon_button/animated_icon_button.dart';
+import 'package:raag/DarkThemeProvider.dart';
+import 'package:provider/provider.dart';
+
+class ThemeButton extends StatefulWidget {
+  @override
+  _ThemeButtonState createState() => _ThemeButtonState();
+}
+
+class _ThemeButtonState extends State<ThemeButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(milliseconds: 400),
+      vsync: this,
+    );
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+    final Color themeIconcolor = Theme.of(context).accentColor;
+    //Color(0xFF809DF5);
+
+    return AnimatedIconButton(
+      animationController: controller,
+      startIcon: Icon(
+        Icons.nights_stay,
+        color: themeIconcolor,
+      ),
+      endIcon: Icon(
+        Icons.wb_sunny_outlined,
+        color: themeIconcolor,
+      ),
+      onPressed: () {
+        if (themeChange.darkTheme == true) {
+          themeChange.darkTheme = false;
+        } else {
+          themeChange.darkTheme = true;
+        }
+        setState(() {
+          themeChange.darkTheme ? controller.forward() : controller.reverse();
+        });
+      },
+    );
+  }
+}
