5ec025576f4a2981e00843d6bd93ece79e6a242f
YUVRAJ SINGH YADAV <73466487+yuvi0069@users.noreply.github.com>
2020-12-17 20:55:20 +0530

    Screen issue
    just removing appbar thing from here

--- lib/view/home_scaffold.dart
+++ lib/view/home_scaffold.dart
@@ -4,41 +4,23 @@ import 'package:provider/provider.dart';
 import 'package:raag/DarkThemeProvider.dart';
 import 'package:raag/view/My_Music_List.dart';
 
-class HomeScaffold extends StatelessWidget {
-  final IconData themeButton = Icons
-      .wb_sunny_outlined; //TODO Must be changed to night icon when theme is light
+class HomeScaffold extends StatefulWidget {
+  @override
+  _HomeScaffoldState createState() => _HomeScaffoldState();
+}
+
+class _HomeScaffoldState extends State<HomeScaffold> {
+  final IconData themeButton = Icons.wb_sunny_outlined;
+  @override
+  void initState() {
+    super.initState();
+
+    
+  }
 
   @override
   Widget build(BuildContext context) {
-    final themeChange = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          elevation: 0,
-          actions: <Widget>[
-            FlatButton(
-              onPressed: () {
-                if (themeChange.darkTheme == true) {
-                  themeChange.darkTheme = false;
-                } else {
-                  themeChange.darkTheme = true;
-                }
-              },
-              child: Icon(
-                themeButton,
-                color: Theme.of(context).accentColor,
-              ),
-            ),
-          ],
-          title: Center(
-              child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Image.asset(
-              'assets/images/musical.png',
-              width: 40,
-              height: 40,
-            ),
-          ))),
       body: Stack(
         children: [
           SafeArea(
