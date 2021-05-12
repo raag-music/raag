bbc37ea262a64bfd0c979d91a355b6068f53c98f
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2021-05-12 23:46:29 +0530

    feat: Use ffmpmeg to convert webm to mp3 (support mp3 headers)

--- lib/provider/audio_helper.dart
+++ lib/provider/audio_helper.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:math';
 
+import 'package:audiotagger/audiotagger.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 AnimationController playFABController;
 
@@ -28,7 +34,6 @@ String formatDuration(Duration d) {
   return format;
 }
 
-
 bool isValidYouTubeURL(String url) {
   url =
       url.replaceAll('://m.y', '://www.y'); //For changing mobile URL to web URL
@@ -40,4 +45,36 @@ bool isValidYouTubeURL(String url) {
     return false;
   else
     return true;
+}
+
+Future<File> urlToFile(String imageUrl) async {
+  //https://mrgulshanyadav.medium.com/convert-image-url-to-file-format-in-flutter-10421bccfd18
+  var random = new Random();
+  Directory tempDir = await getTemporaryDirectory();
+  String tempPath = tempDir.path;
+  File file = new File('$tempPath' + (random.nextInt(100)).toString() + '.png');
+  http.Response response = await http.get(Uri.parse(imageUrl));
+  await file.writeAsBytes(response.bodyBytes);
+  return file;
+}
+
+Future<String> webmToMP3(String path) async {
+  final FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
+  int result = 0;
+  final newPath = path.replaceAll('.webm', '_RaagApp.mp3');
+  var command = "-i \"$path\" -vn -ab 128k -ar 44100 -y \"$newPath\"";
+  await _flutterFFmpeg.execute(command).then((rc) {
+    print('FFmpeg Result: $rc');
+    result = rc;
+  });
+  await File(path).delete();
+  return newPath;
+}
+
+Future<bool> tagArtWork(String url, String path) async {
+  var tagger = new Audiotagger();
+  File albumArtFile = await urlToFile(url);
+  final result = await tagger.writeTag(
+      path: path, tagField: "artist", value: 'Artist');
+  return result;
 }
